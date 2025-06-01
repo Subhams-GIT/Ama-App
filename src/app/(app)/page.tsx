d0954ad5da113badfae6c750921973bb596cb648
Subhams-GIT <subhamkumardas4444@gmail.com>
2025-06-01 18:30:30 +0530

    fixed render error

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -13,15 +13,17 @@ import {
 } from '@/components/ui/carousel';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
 
   const router=useRouter();
   const {data: session} = useSession();
-  if(session?.user)
-  {
-    router.replace('/dashboard')
+ useEffect(() => {
+  if (session?.user) {
+    router.replace('/dashboard');
   }
+}, [session, router]);
   return (
     <>
       {/* Main content */}
