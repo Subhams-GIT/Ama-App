35840857e8566c71f5f8f2c956f14721a3390b81
Subhams-GIT <subhamkumardas4444@gmail.com>
2025-06-03 12:33:46 +0530

    removed vars

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -3,8 +3,7 @@ import {Geist, Geist_Mono} from "next/font/google";
 import ".././globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import NavBar from "@/components/NavBar";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/options";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
